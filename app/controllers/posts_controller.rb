f9d52fceb863e46bb7b6b078554a11429cfa05e8
Gavin Pavlick <gavin.pavlick@kellogg.northwestern.edu>
2023-05-15 23:52:32 +0000

    today first commit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,25 @@
 class PostsController < ApplicationController
-  def index
+  def new
+    @post = Post.new
+    @post["place_id"] = params["place_id"]
   end
+
+  
+  def create
+    #start with new Place
+    @post = Post.new
+
+    #assign user-entered form data to Place columns
+    @post["title"] = params["post"]["title"]
+    @post["description"] = params["post"]["description"]
+    @post["posted_on"] = params["post"]["posted_on"]
+    @post["place_id"] = params["post"]["place_id"]
+
+    #save Post row
+    @post.save
+
+    #redirect user
+    redirect_to "/places/#{@post["place_id"]}"
+  end
+
 end
