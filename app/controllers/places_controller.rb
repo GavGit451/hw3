e47d2a657a6f422c7b5c7eed5dd3b65891184d18
Gavin Pavlick <gavin.pavlick@kellogg.northwestern.edu>
2023-05-15 03:31:58 +0000

    tonight 2 commit

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,4 +1,15 @@
 class PlacesController < ApplicationController
   def index
-  end 
+    # find all Place rows
+    @places = Place.all
+    #render companies/index view
+  end
+
+  def show
+    # find a Place
+    @place = Place.find_by({"id" => params["id"]})
+    # render places/show view with details
+  end
+
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+    # places = Place.create([{ name: "Mexico" }])
+
+    # apple = Company.new
+    # apple["name"] = "Apple"
+    # apple["city"] = "Cupertino"
+    # apple["state"] = "CA"
+    # apple.save
+    
+      # charleston = Place.new
+      # charleston["name"] = "Charleston"
+      # charleston.save
+
+
+      # ate_tacos = Post.new
+      # ate_tacos["title"] = "Ate Tacos"
+      # ate_tacos["description"] = "Ate Tacos while in Mexico"
+      # ate_tacos["place_id"] = charleston["id"]
+      # ate_tacos.save
+
+
+    # tim = Contact.new
+    # tim["first_name"] = "Tim"
+    # tim["last_name"] = "Cook"
+    # tim["email"] = "tim@apple.com"
+    # tim["phone_number"] = "777-777-7777"
+    # tim["company_id"] = apple["id"]
+    # tim.save
